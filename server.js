f370ff3b2b6c49f1d5bc64cd0909302eb33c9168
pramodkmr73 <30777569+pramodkmr73@users.noreply.github.com>
2017-08-26 21:10:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function (req, res) {
+  res.send('Article one Requested');
+});
+
+app.get('/article-two', function (req, res) {
+  res.send('Article two Requested');
+});
+
+app.get('/article-three', function (req, res) {
+  res.send('Article three Requested');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
